1e45f432b140dc235f23f3237171b2076b00cc41
AlfonsoMakajumba <juliusz129@gmail.com>
2026-06-17 22:34:23 +0200

    added init of SysTick

--- Src/main.c
+++ Src/main.c
@@ -27,6 +27,9 @@ int main(void)
     RCC -> AHB1ENR |= (1 << 0); //enables clock for GPIOA, RM0390 p. 142/1321
     GPIOA -> MODER &= ~(0b11 << 10); //clears bit 10 and 11 in MODER register, RM0390 p. 185/1321
     GPIOA -> MODER |= (0b01 << 10); //sets bit 10 and 11 to 01 accordingly, RM0390 p. 185/1321
+
+    SysTick_init();
+
 	while(1)
     {
         GPIOA -> ODR ^= (1 << 5); //sets PA5 to high if its low and to low if its high, RM0390 p. 187/1321
